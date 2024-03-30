edec37cbc471033b9dc8ea407b8345ece3656cae
AJ Schulte <captrockstar007@gmail.com>
2024-03-30 12:01:02 -0500

    Added up/down movement and continued work on level 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,18 +19,26 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var x_direction = Input.get_axis("ui_left", "ui_right")
+	if x_direction:
+		velocity.x = x_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
+	var y_direction = Input.get_axis("ui_up", "ui_down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
 	if velocity.x > 0 or velocity.x < 0 :
 		$AnimatedSprite2D.play("walk")
 		if velocity.x < 0:
 			$AnimatedSprite2D.flip_h = true
 		else:
 			$AnimatedSprite2D.flip_h = false
+	elif velocity.y != 0:
+		$AnimatedSprite2D.play("walk")
 	else:
 		$AnimatedSprite2D.play("idle")
 

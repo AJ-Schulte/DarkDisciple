4cc8c9a9da8296a13e320baff8841ec17cc4beeb
Nturner10 <156948502+Nturner10@users.noreply.github.com>
2024-04-26 22:38:39 -0500

    Title Screen, Credits, and Buttons all finished

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,10 +2,16 @@ extends Node
 
 @export var game_scene = "res://scenes/game.tscn"
 var game_world:Node2D
+var credits:Control
+
 
 func _on_start_game_pressed():
 	game_world=load(game_scene).instantiate()
 	add_child(game_world)
+	$StartGame.disabled = true 
+	$Quit.disabled = true
+	$CreditsButton.disabled = true 
+	
 
 func open_main_menu():
 	
@@ -15,3 +21,24 @@ func open_main_menu():
 	var main_menu=load("res://scenes/menu/main_menu.tscn").instantiate()
 	add_child(main_menu)
 	main_menu.starting.connect(_on_start_game_pressed)
+
+func quit_game():
+	get_tree().quit()
+
+func _on_quit_pressed():
+	call_deferred("quit_game")
+
+
+
+func _on_credits_pressed():
+	credits = load("res://scenes/credits.tscn").instantiate()
+	add_child(credits)
+	$Timer.start()
+
+
+func _on_timer_timeout():
+	call_deferred("remove_child", $Credits)
+	$Timer.stop()
+	
+
+
